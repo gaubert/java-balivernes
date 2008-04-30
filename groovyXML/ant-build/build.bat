7ea236dba5cd0ef2fc2898f342cca798669eb243
aubert <aubert@aubert-desktop.(none)>
2008-04-30 15:31:04 +0200

    Add ant files

---
+++ groovyXML/ant-build/build.bat
@@ -0,0 +1,10 @@
+@echo off
+rem -------------------------------------------------------------------------
+rem File: build.bat
+rem
+rem Purpose: commands to build the ECAccess project
+rem
+rem Copyright (c) 2003 ECMWF, NSS. All Rights Reserved
+rem -------------------------------------------------------------------------
+
+..\tools\bin\ant.bat %1
