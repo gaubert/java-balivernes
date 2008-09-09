4a86e44047081f1323fd41ff0164fc385ae0ca86
aubert <aubert@aubert-desktop.(none)>
2008-09-09 10:47:54 +0200

    add missing elements

---
+++ RNpicker/src/common/xml_utils.py
@@ -0,0 +1,18 @@
+import common.utils
+from lxml import etree
+
+
+
+def pretty_print_xml(aFDescriptor,aOutput):
+   """ xml pretty printing from a stream. Take a file descriptor (fd or StringIO for example """
+   
+   tree = etree.parse(aFDescriptor)
+   
+   # get xslt stylesheet doing the transformation
+   xsltPath = common.utils.Conf.get_conf().get("Transformer","xsltPrettyPrinter")
+   
+   transform = etree.XSLT(etree.parse(open(xsltPath)))
+   
+   result = transform(tree)
+   
+   common.utils.printInFile(str(result),aOutput)
