33d5c7fd4faa61063f804b56c25920c3017933b9
aubert <guillaume.aubert@gmail.com>
2009-01-07 18:07:26 +0100

    separate conf from RNPicker on order to create a different module

--- RNpicker/dists/conf-dist/setup.py
+++ RNpicker/dists/conf-dist/setup.py
@@ -1,23 +1,23 @@
 import os
 from setuptools import setup, find_packages
-version = '0.1.0'
+version = '0.8'
 README = os.path.join(os.path.dirname(__file__), 'README.txt')
 long_description = open(README).read() + 'nn'
-setup(name='RNPicker',
+setup(name='conf',
       version=version,
-      description=("Software for exporting the RN Data as XML, "
+      description=("Conf a class for powerfully accessing configuration files, "
                     "from CTBTO (www.ctbto.org)"),
       long_description=long_description,
       classifiers=[
         "Programming Language :: Python",
-        ("Topic :: RadioNuclide Processing :: Libraries :: Python Modules"),
+        ("Topic :: Util :: Libraries :: Python Modules"),
         ],
-      keywords='radionuclide xml ',
+      keywords='configuration, resources',
       author='Guillaume Aubert',
       author_email='guillaume.aubert@ctbto.org',
       url='http://www.ctbto.org',
       license='Apache 2.0',
       packages=find_packages(),
-      namespace_packages=['ctbto'],
-      install_requires=['SQLAchemy>=0.5.0','cx-Oracle>=','lxml>=2.0']
+      namespace_packages=['org'],
+      install_requires=[]
       )
---
+++ RNpicker/dists/rnpicker-dist/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+version = '0.1.0'
+README = os.path.join(os.path.dirname(__file__), 'README.txt')
+long_description = open(README).read() + 'nn'
+setup(name='RNPicker',
+      version=version,
+      description=("Software for exporting the RN Data as XML, "
+                    "from CTBTO (www.ctbto.org)"),
+      long_description=long_description,
+      classifiers=[
+        "Programming Language :: Python",
+        ("Topic :: RadioNuclide Processing :: Libraries :: Python Modules"),
+        ],
+      keywords='radionuclide xml ',
+      author='Guillaume Aubert',
+      author_email='guillaume.aubert@ctbto.org',
+      url='http://www.ctbto.org',
+      license='Apache 2.0',
+      packages=find_packages(),
+      namespace_packages=['ctbto'],
+      install_requires=['SQLAchemy>=0.5.0','cx-Oracle>=4.3','lxml>=2.0']
+      )
