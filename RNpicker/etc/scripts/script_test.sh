bdf13f52d1879cbefa4aa4b7acf04555eb834c56
aubert <guillaume.aubert@gmail.com>
2009-01-19 16:23:32 +0100

    improve error handling

--- RNpicker/etc/scripts/script_test.sh
+++ RNpicker/etc/scripts/script_test.sh
@@ -23,31 +23,43 @@ PYTHON_BIN=$RNPICKER_HOME/bin/python
 # /usr/lib/oracle/xe/app/oracle/product/10.2.0/server
 DEFAULT_ORACLE_HOME=/usr/lib/oracle/xe/app/oracle/product/10.2.0/server
 
+#DEFAULT_LIBXML2_DIR=/home/smd/aubert/new-env/ext/clib/libxml2/lib
 DEFAULT_LIBXML2_DIR=/usr/lib
 
+#DEFAULT_LIBXSLT_DIR=/home/smd/aubert/new-env/ext/clib/libxslt/lib
 DEFAULT_LIBXSLT_DIR=/usr/lib
+
+DEFAULT_RNPICKER_CONF_DIR=$RNPICKER/conf
 
 #################################
 ## You probably do not want to
 ## change anything from there
 #################################
 
+echo "INFO - *************************************************************"
+echo "INFO - Check configuration and environment"
+
 ##################################
 ## Apply Default Settings if global not defined
 ##################################
 if [ -z "$ORACLE_HOME" ]; then
-   echo "ORACLE_HOME is undefined. Set it to default=($DEFAULT_ORACLE_HOME)"
+   echo "INFO - ORACLE_HOME is undefined. Set it to default=($DEFAULT_ORACLE_HOME)."
    ORACLE_HOME=$DEFAULT_ORACLE_HOME
 fi
 
 if [ -z "$LIBXML2_DIR" ]; then
-   echo "LIBXML2_DIR is undefined. Set it to default=($DEFAULT_LIBXML2_DIR)"
+   echo "INFO - LIBXML2_DIR is undefined. Set it to default=($DEFAULT_LIBXML2_DIR)."
    LIBXML2_DIR=$DEFAULT_LIBXML2_DIR
 fi
 
 if [ -z "$LIBXSLT_DIR" ]; then
-   echo "LIBXSLT_DIR is undefined. Set it to default=($DEFAULT_LIBXSLT_DIR)"
+   echo "INFO - LIBXSLT_DIR is undefined. Set it to default=($DEFAULT_LIBXSLT_DIR)."
    LIBXSLT_DIR=$DEFAULT_LIBXSLT_DIR
+fi
+
+if [ -z "$RNPICKER_CONF_DIR" ]; then
+   echo "INFO - RNPICKER_CONF_DIR is undefined. Set it to default=($DEFAULT_RNPICKER_CONF_DIR)."
+   RNPICKER_CONF_DIR=$DEFAULT_RNPICKER_CONF_DIR
 fi
 
 ####################################
@@ -55,7 +67,7 @@ fi
 ####################################
 
 if [ ! -f "$ORACLE_HOME/lib/libclntsh.so" ]; then
-    echo "ORACLE_HOME: [$ORACLE_HOME] is not an oracle distribution"
+    echo "ERROR - ORACLE_HOME: [$ORACLE_HOME] is not an oracle distribution"
     echo "Please set the ORACLE_HOME variable globally or set DEFAULT_ORACLE_HOME (defined in this script) to an oracle distribution"
     exit 1
 else
@@ -63,7 +75,7 @@ else
 fi
 
 if [ ! -f "$LIBXML2_DIR/libxml2.so" ]; then
-    echo "LIBXML2_DIR: [$LIBXML2_DIR] doesn\'t contain libxml2.so"
+    echo "ERROR - LIBXML2_DIR: [$LIBXML2_DIR] doesn\'t contain libxml2.so"
     echo "Please set the LIBXML2_DIR variable globally or set DEFAULT_LIBXML2_DIR (defined in this script) to a directory containing libxml2.so"
     exit 1
 else
@@ -71,22 +83,18 @@ else
 fi
 
 if [ ! -f "$LIBXSLT_DIR/libxslt.so" -a ! -f "$LIBXSLT_DIR/lib/libexslt.so" ]; then
-    echo "LIBXSLT_DIR: [$LIBXSLT_DIR] doesn\'t contain libexslt.so"
+    echo "ERROR - LIBXSLT_DIR: [$LIBXSLT_DIR] doesn\'t contain libexslt.so"
     echo "Please set the LIBXSLT_DIR variable globally or set DEFAULT_LIBXSLT_DIR (defined in this script) to a directory containing libxslt.so and libexslt.so"
     exit 1
 else
     LD_LIBRARY_PATH=$LIBXSLT_DIR:$LD_LIBRARY_PATH 
 fi
 
+if [ ! -f "$RNPICKER_CONF_DIR/rnpicker.config" ]; then
+    echo "RNPICKER_CONF $RNPICKER_CONF_DIR doesn't contain rnpicker.config. generate_arr will try to load it from the CLI argument --conf_dir"
+fi
+
 export LD_LIBRARY_PATH
-
-
-echo "LD_LIBRARY_PATH $LD_LIBRARY_PATH"
-echo "RNPICKER_HOME distribution directory $RNPICKER_HOME"
-
-
-
-
 
 ###################################
 ## create bootstrap and launch program
@@ -102,6 +110,8 @@ cat > /tmp/rnpicker.bootstrap << EOF
 import ctbto.run.generate_arr as runner
 runner.run()
 EOF
+
+echo "INFO - *************************************************************"
 
 #./generate_arr $ARGS
 $PYTHON_BIN /tmp/rnpicker.bootstrap $ARGS
