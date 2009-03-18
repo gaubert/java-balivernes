cd09ca6052c6c5efacfde1f0f08fb0ffb8c2e9a3
aubert <guillaume.aubert@gmail.com>
2009-03-18 15:09:15 +0100

    add db path in dir

--- RNpicker/etc/ext/crontab/crontab_runner.sh
+++ RNpicker/etc/ext/crontab/crontab_runner.sh
@@ -3,6 +3,7 @@
 #export LD_LIBRARY_PATH=/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/lib::/usr/local/lib
 . /usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin/oracle_env.sh
 export TNS_ADMIN=/home/aubert
+CRN_HOME=/home/aubert/RNPickerEnv
 
 logging_dir=/tmp/cronlogs
 mkdir -p $logging_dir
@@ -16,7 +17,7 @@ echo "Starting Run at $begin_date"
 echo "outputs for this run are in $output"
 echo "outputs for this run are in $output" >> $output
 
-/tmp/new-env/bin/generate_products_and_email -g test >> $output 2>&1
+$CRN_HOME/new-env/bin/generate_products_and_email -g test >> $output 2>&1
 result="$?"
 
 end_date=`date +"%m-%d-%yT%T"`
