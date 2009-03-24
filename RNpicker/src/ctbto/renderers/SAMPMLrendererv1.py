2c0b11f9a414341993ca0fbd9de0ad00f336342b
aubert <guillaume.aubert@gmail.com>
2009-03-24 17:13:01 +0100

    fix QC Flags for Spalax

--- RNpicker/src/ctbto/renderers/SAMPMLrendererv1.py
+++ RNpicker/src/ctbto/renderers/SAMPMLrendererv1.py
@@ -466,16 +466,25 @@ class SpalaxRenderer(BaseRenderer):
         # add Data Quality Flags
         dataQFlags = self._fetcher.get('%s_QC_FLAGS' % (id), [])
         
+        dataQFlags.sort()
+        
         xml = ''
         
         # get the template for a unique flag
         one_flag = self._conf.get("SpalaxTemplatingSystem", "spalaxQCFlagTemplate")
         
+        l = []
+        
         for dFlag in dataQFlags:
             xml_f = re.sub("\${NAME}", dFlag[u'TEST_NAME'], one_flag)
             xml_f = re.sub("\${COMMENT}",dFlag[u'QC_COMMENT'], xml_f)
             xml_f = re.sub("\${PASS}","true" if dFlag[u'FLAG'] == 'G' else "false", xml_f)
-            xml += xml_f
+            l.append(xml_f)
+        
+        # sort the list l alphabetically 
+        l.sort() 
+         
+        xml = ''.join(l)
         
         return  re.sub("\${QC_FLAGS}",xml,template)   
     
@@ -694,96 +703,6 @@ class SpalaxRenderer(BaseRenderer):
         # out of the loop
         self._populatedTemplate = re.sub("\${CALIBRATION}", xml, self._populatedTemplate)
     
-    
-    def _oldfillData(self, requestDict):
-        """ Insert the spectrum data expected as defined in the initial passed request
-        
-            Args:
-               requestDict: dictionary representing the different elements of the request (analysis, spectrum, ...)
-            
-            Returns: Nothing
-              
-              
-            Raises:
-               exception if issue fetching data (CTBTOError)
-        """
-    
-        # check if there is a spectrum in the hashtable. If not replace ${SPECTRUM} by an empty string ""
-        requestedTypes = requestDict[RequestParser.SPECTRUM]
-        
-        if 'PREL' in requestedTypes:
-            # add all prels found in CURR_List_OF_PRELS in the set
-            requestedTypes.remove('PREL')
-            requestedTypes.update(self._fetcher.get(u'CURR_List_OF_PRELS', []))
-        
-        spectrums = self._sortSpectrumsSet(requestedTypes)
-        
-        
-        finalTemplate = ""
-        
-      
-        for ty in spectrums:
-            
-            spectrumTemplate = ""
-            
-            # add Spectrum
-            fname = "%s_G_DATA" % (ty)
-              
-            data = self._fetcher.get(fname, None)
-            
-            if data is not None:
-               
-                spectrumTemplate = self._conf.get("SpalaxTemplatingSystem", "spalaxSpectrumTemplate")
-              
-                # insert data
-                spectrumTemplate = re.sub("\${SPECTRUM_DATA}", data, spectrumTemplate)
-              
-                # insert spectrum ID
-                spectrumTemplate = re.sub("\${SPECTRUM_ID}", self._fetcher.get("%s_ID" % (fname)), spectrumTemplate)
-            
-                # insert energy and channel span
-                spectrumTemplate = re.sub("\${SPECTRUM_DATA_CHANNEL_SPAN}", str(self._fetcher.get("%s_CHANNEL_SPAN" % (fname))), spectrumTemplate)
-                spectrumTemplate = re.sub("\${SPECTRUM_DATA_ENERGY_SPAN}", str(self._fetcher.get("%s_ENERGY_SPAN" % (fname))), spectrumTemplate)
-            
-                # get the date information (for the moment it is repeated for each data (beta and gamma spectra and histogram)
-                spectrumTemplate = re.sub("\${COL_START}", str(self._fetcher.get("%s_COLLECT_START" % (fname))), spectrumTemplate)
-                spectrumTemplate = re.sub("\${COL_STOP}", str(self._fetcher.get("%s_COLLECT_STOP" % (fname))), spectrumTemplate)
-                
-                spectrumTemplate = re.sub("\${ACQ_START}", str(self._fetcher.get("%s_ACQ_START" % (fname))), spectrumTemplate)
-                spectrumTemplate = re.sub("\${ACQ_STOP}", str(self._fetcher.get("%s_ACQ_STOP" % (fname))), spectrumTemplate)
-                spectrumTemplate = re.sub("\${SAMPLING_TIME}", str(self._fetcher.get("%s_SAMPLING_TIME" % (fname))), spectrumTemplate)
-                spectrumTemplate = re.sub("\${REAL_ACQ_TIME}", str(self._fetcher.get("%s_ACQ_REAL_SEC" % (fname))), spectrumTemplate)
-                spectrumTemplate = re.sub("\${LIVE_ACQ_TIME}", str(self._fetcher.get("%s_ACQ_LIVE_SEC" % (fname))), spectrumTemplate)
-                spectrumTemplate = re.sub("\${ARRIVAL_DATE}", str(self._fetcher.get("%s_TRANSMIT_DTG" % (fname))), spectrumTemplate)
-              
-                spectrumTemplate = re.sub("\${DECAY_TIME}", str(self._fetcher.get("%s_DECAY_TIME" % (fname))), spectrumTemplate)
-                spectrumTemplate = re.sub("\${SPECTRUM_TYPE}", str(self._fetcher.get("%s_SPECTRAL_QUALIFIER" % (fname))), spectrumTemplate)
-                spectrumTemplate = re.sub("\${MEASUREMENT_TYPE}", str(self._fetcher.get("%s_DATA_TYPE" % (fname))), spectrumTemplate)
-                # add quantity and geometry
-                spectrumTemplate = re.sub("\${QUANTITY}", str(self._fetcher.get("%s_SAMPLE_QUANTITY" % (fname))), spectrumTemplate)
-                spectrumTemplate = re.sub("\${FLOW_RATE}", str(self._fetcher.get("%s_FLOW_RATE" % (fname))), spectrumTemplate)
-                
-                spectrumTemplate = re.sub("\${GEOMETRY}", str(self._fetcher.get("%s_SAMPLE_GEOMETRY" % (fname))), spectrumTemplate)
-              
-                l = self._fetcher.get("%s_ALL_CALS" % (fname))
-                if l is None:
-                    SpalaxRenderer.c_log.warning("No calibration information for sample %s" % (ty))
-                    l = []
-                
-                # add calibration info
-                spectrumTemplate = re.sub("\${CAL_INFOS}", ' '.join(map(str, l)), spectrumTemplate) #IGNORE:W0141
-              
-                # TODO to remove just there for testing, deal with the compression flag
-                if self._fetcher.get("%s_COMPRESSED" % (fname), False):
-                    spectrumTemplate = re.sub("\${COMPRESS}", "compress=\"base64,zip\"", spectrumTemplate)
-                else:
-                    spectrumTemplate = re.sub("\${COMPRESS}", "", spectrumTemplate)
-                     
-                # add fill spectrum template in global template 
-                finalTemplate += spectrumTemplate
-        
-        self._populatedTemplate = re.sub("\${DATA}", finalTemplate, self._populatedTemplate)
-
     def _fillData(self, requestDict):
         """ Insert the spectrum data expected as defined in the initial passed request
         
