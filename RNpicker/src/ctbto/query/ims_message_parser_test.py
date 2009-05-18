161d46503ee95fc7299fc2d18088045a9ad8c0e4
aubert <guillaume.aubert@gmail.com>
2009-05-18 21:51:03 +0200

    add first parsing validation

--- RNpicker/src/ctbto/query/ims_message_parser_test.py
+++ RNpicker/src/ctbto/query/ims_message_parser_test.py
@@ -4,8 +4,6 @@ Created on May 18, 2009
 @author: guillaume.aubert@ctbto.org
 '''
 
-import StringIO
-
 from unittest import TestCase,TestLoader,TextTestRunner
 
 from ims_message_parser import IMSParser
@@ -16,4 +14,12 @@ class IMSMessageParserTest(TestCase):
         pass
         
     def test_list_tokens_lower_case(self):
-        pass
+        
+        
+        message = "     begin IMS2.0     \nmsg_type data \nmsg_id 54695 ctbto_idc\ne-mail guillaume.aubert@ctbto.org     \ntime 2000/11/22 to 2001/01/01\nsta_list ARP01\nalert_temp\n    stop"
+        
+        parser = IMSParser()
+        
+        result = parser.parse(message)
+        
+        print("Result = %s\n"%(result))
