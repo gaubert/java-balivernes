25d6611630b3a823c1bd3be82855502b2cd85c0c
aubert <guillaume.aubert@gmail.com>
2009-07-31 17:59:45 +0200

    add station name in netcdf profile

--- InfraProfiler/src/create_netcdf.py
+++ InfraProfiler/src/create_netcdf.py
@@ -11,25 +11,29 @@ dataset.createDimension('longitude', 1)
 dataset.createDimension('latitude', 1)
 dataset.createDimension('time', 1)
 dataset.createDimension('altitude', 361)
+dataset.createDimension('loc_name', 1)
 
 #create basic variables
-the_time     = dataset.createVariable('time','f8',('time'))
-lat       = dataset.createVariable('latitude','f4',('latitude'))
-lon       = dataset.createVariable('longitude','f4',('longitude'))
-altitudes = dataset.createVariable('altitude','f4',('altitude'))
+the_time  = dataset.createVariable('time', 'f8', ('time'))
+lat       = dataset.createVariable('latitude', 'f4', ('latitude'))
+lon       = dataset.createVariable('longitude', 'f4', ('longitude'))
+altitudes = dataset.createVariable('altitude', 'f4', ('altitude'))
+
+# create loc_name
+loc_name  = dataset.createVariable('loc_name', str,('loc_name'))
 
 # create param variables
 # temperature
-t         = dataset.createVariable('t','f4',('time', 'altitude', 'latitude', 'longitude'))
+t         = dataset.createVariable('t','f4',('time', 'altitude', 'latitude', 'longitude', 'loc_name'))
 # u and v wind components
-u         = dataset.createVariable('u','f4',('time', 'altitude', 'latitude', 'longitude'))
-v         = dataset.createVariable('v','f4',('time', 'altitude', 'latitude', 'longitude'))
+u         = dataset.createVariable('u','f4',('time', 'altitude', 'latitude', 'longitude', 'loc_name'))
+v         = dataset.createVariable('v','f4',('time', 'altitude', 'latitude', 'longitude', 'loc_name'))
 # log surface pressure
-lnsp      = dataset.createVariable('lnsp','f4',('time', 'altitude', 'latitude', 'longitude'))
+lnsp      = dataset.createVariable('lnsp','f4',('time', 'altitude', 'latitude', 'longitude', 'loc_name'))
 # Q (humidity)
-q         = dataset.createVariable('q','f4',('time', 'altitude', 'latitude', 'longitude'))
+q         = dataset.createVariable('q','f4',('time', 'altitude', 'latitude', 'longitude', 'loc_name'))
 # geopotential (no altitude)
-z         = dataset.createVariable('z','f4',('time', 'latitude', 'longitude'))
+z         = dataset.createVariable('z','f4',('time', 'latitude', 'longitude', 'loc_name'))
 
 #dataset.sync()
 
@@ -38,13 +42,15 @@ dataset.description = 'CTBTO Infrasound wind profiles'
 dataset.history     = 'Created ' + time.ctime(time.time()) + ' by infra-profile-generator-v1.2.2'
 dataset.source      = 'infra-profile-generator-v1.2.2'
 dataset.version     = 'infrasound profile v1.0-20090801'
-dataset.station     = 'IS42'
+#dataset.station     = 'IS42'
 lat.units           = 'degrees north'
 lat.long_name       = 'Latitude'
 lon.units           = 'degrees east'
 lon.long_name       = 'Longitude'
 altitudes.units     = 'm'
 altitudes.long_name = 'Altitude'
+loc_name.units      = '-'
+loc_name.long_name  = 'Location name'
 the_time.units      = 'hours since 0001-01-01 00:00:00.0'
 the_time.calendar   = 'gregorian'
 the_time.long_name  = 'Time'
