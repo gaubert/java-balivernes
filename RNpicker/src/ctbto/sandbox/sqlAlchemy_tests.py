30b850c0f19c284ffd6a8e72a4df2ded84194037
aubert <guillaume.aubert@gmail.com>
2009-08-05 14:30:55 +0200

    test with SQLAclhemy

--- RNpicker/src/ctbto/sandbox/sqlAlchemy_tests.py
+++ RNpicker/src/ctbto/sandbox/sqlAlchemy_tests.py
@@ -57,12 +57,53 @@ def getMetadata():
 def printDict(di, format="%-25s %s"):
     for (key, val) in di.items():
         print format % (str(key)+':', val)
+        
+
+def test_with_metadata():
+    
+    print "Connect to test database"
+
+    engine = create_engine('oracle://centre:data@idcdev', echo=False)
+
+    conn = engine.connect()
+
+    print "Connected to test database"
+
+    # create MetaData 
+    meta = MetaData()
+
+    # bind to an engine
+    meta.bind = engine
+    
+    #fileproduct = Table('FILEPRODUCT', meta, autoload=True)
+    fileproduct = Table('FILEPRODUCT', meta, autoload=True)
+    
+    the_schema = 'sel3'
+    
+    sel3_origin   = Table('ORIGIN',meta, schema= the_schema, autoload=True)
+    #sel3_assoc    = Table('SEL3.ASSOC',meta, autoload=True)
+    #sel3_origerr  = Table('SEL3.ORIGERR',meta, autoload=True)
+
+    the_table = sel3_origin
+
+    cols = [] 
+
+    for c in the_table.columns:
+        desc = {}
+        desc['name'] = c.name
+        desc['type'] = c.type
+        cols.append(desc)
+    
+    print("Cols %s\n"%(cols))
+    
+def test_with_metadata_and_select():
+    
 
 
 def main():
     print "Hello %d, string = %s"%(1,"my String")
 
-    getMetadata()
+    test_with_metadata()
 
 
 
