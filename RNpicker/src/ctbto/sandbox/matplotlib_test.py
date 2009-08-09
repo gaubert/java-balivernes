cd055d92c9f0fc5ae384ee412ecec70071968e6c
aubert <guillaume.aubert@gmail.com>
2009-08-09 15:18:40 +0200

    intermediate progress on metastable plot

--- RNpicker/src/ctbto/sandbox/matplotlib_test.py
+++ RNpicker/src/ctbto/sandbox/matplotlib_test.py
@@ -73,6 +73,72 @@ def test_time():
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%b-%d %H:%M')) 
     plt.show()
 
+def another_time_example():
+    """
+    from datetime import date
+    from matplotlib.pyplot import plot, show, title, \
+         xlabel, ylabel, set, gca, bar, savefig
+
+    d = {date(2003,11,1) : 12,
+         date(2003,11,2) : 20,
+         date(2003,11,5) : 20,
+         date(2003,11,9) : 18,
+         date(2003,11,12) : 2,
+         }
+
+    dates = [ (date, count) for date, count in d.items()]
+    dates.sort()
+    labels = [date.strftime('%b %d') for date, count in dates]
+
+    dates, counts = zip(*dates)  # split to two lists
+    days = [date.day for date in dates]  # get the day
+    plot(days, counts)
+    set(gca(), 'xticks', days)  # force the ticks to fall on the days
+    set(gca(), 'xticklabels', labels)  # force the ticks to fall on the days
+    ylabel('count')
+
+    show()
+    """
+    pass
+
+def time2():
+    import datetime
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.dates as mpldates
+    import matplotlib.ticker as ticker
+    
+    date1 = datetime.date( 1952, 1, 1 )
+    date2 = datetime.date( 2004, 4, 12 )
+    delta = datetime.timedelta(days=100)
+    dates = mpldates.drange(date1, date2, delta)
+    s1 = np.random.rand(len(dates))
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot_date(dates, s1,'r.')
+    ax.hold(True)
+    s2 = np.random.rand(len(dates))
+    ax.plot_date(dates, s2,'bo')
+    ax.legend(('s1','s2'), numpoints=1)
+    
+    # only write ticklabels on the decades
+    def fmtticks(x, pos=None):
+        dt = mpldates.num2date(x)
+        if dt.year%10: return ''
+        return dt.strftime('%Y')
+    
+    # this fizes yhe tick labels
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(fmtticks))
+    
+    # this fixes the toolbar x coord
+    ax.fmt_xdata = mpldates.DateFormatter('%Y-%m-%d')
+    
+    # this rotates the ticklabels to help with overlapping
+    fig.autofmt_xdate()
+    plt.show()
+
+
 def test_my_plot():
     
     import matplotlib.pyplot as plt
@@ -80,26 +146,96 @@ def test_my_plot():
     import matplotlib.dates as mdates
     from matplotlib.dates import YearLocator, MonthLocator, DayLocator, DateFormatter
 
-    
-    print("dates = %s\n" %(dates) )
-    
-    # create plot
-    plt.plot_date(pylab.date2num(dates), XE133_LIGHT_Y, linestyle='-') 
-    # format 
-    xaxis = plt.gca().xaxis
-    xaxis.set_major_locator(DayLocator())
-    #xaxis.set_major_formatter(mdates.DateFormatter('%y-%m-%d-%H:%M')) 
-    xaxis.set_major_formatter(mdates.DateFormatter('%y-%m-%d'))
-    #xaxis.autoscale_view()
 
+    #read csv file and get the data
+    (XE131M_dict, XE133M_dict, XE133_dict) = test_read_csv()
+    
+    # sort the dates
+    sorted_dates   = sorted(set(XE133_dict.keys()))
+    XE133_sorted_values   = []
+    XE131M_sorted_values  = []
+    XE133M_sorted_values  = []
+    
+    for k in sorted_dates:
+        XE133_sorted_values.append(XE133_dict[k])
+        XE131M_sorted_values.append(XE131M_dict[k])
+        XE133M_sorted_values.append(XE133M_dict[k])
+        
+    
+    #print("sorted_dates=%s\n" % (sorted_dates) )
+    
+    # figure
+    fig = plt.figure()
+    plot1 = fig.add_subplot(212)
+    
+    # create plot for XE133
+    plot1.plot_date(pylab.date2num(sorted_dates), XE133_sorted_values, marker='^', color='r') 
+    
+    #plot2.plot_date(pylab.date2num(sorted_dates), sorted_values, marker='^') 
+    # format
+     
+    #xaxis = plot1.gca().xaxis
+    xaxis1 = plot1.xaxis
+    
+    # define xaxis beg and end limits
+    #begin = dateutil.parser.parse('2009-07-31 00:00')
+    #end   = dateutil.parser.parse('2009-08-03 00:00')
+    #plt.xlim( (begin,end) )
+    
+    # format xaxis with dates
+    xaxis1.set_major_locator(MonthLocator())
+    xaxis1.set_major_formatter(mdates.DateFormatter('%b-%y'))
+    
+    # for day formatting
+    #xaxis.set_major_locator(DayLocator())
+    #xaxis.set_major_formatter(mdates.DateFormatter('%y-%m-%d'))
+
+    plot1.set_ylim( (0, 1) )
+    
+    
+    #create plot for metastables
+    plot2 = fig.add_subplot(211)
+    
+    plot2.plot_date(pylab.date2num(sorted_dates), sorted_values, marker='^', color='y') 
+    plot2.plot_date(pylab.date2num(sorted_dates), sorted_values2, marker='*', color='b')
+    
+    xaxis2 = plot1.xaxis
+    
+    xaxis2.set_major_locator(MonthLocator())
+    xaxis2.set_major_formatter(mdates.DateFormatter('%b-%y'))
+    
+    plot2.set_ylim( (0, 3) )
+    
+     # to have tick names in diagonals
+    fig.autofmt_xdate()
+    
+    #save figure
     plt.savefig('/tmp/example.png',format='png')
     
-    #plt.plot_date(dates, XE133_LIGHT_Y, fmt='bo', tz=None, xdate=True, ydate=False)
-
-    #plt.plot([1,2,3,4], [1,4,9,16], 'ro')
-    #plt.axis([0, 6], [0, 20])
     plt.show()
-
+    
+def test_read_csv():
+    import csv
+    csvReader = csv.reader(open('/home/aubert/cnx22-XE133-XE131M-XE133M.csv'), delimiter=',', quotechar='"')
+    
+    XE133_dict  = {}
+    XE131M_dict = {}
+    XE133M_dict = {}
+    
+    # create a dict date -> val
+    cpt = 0
+    for row in csvReader:
+        cpt += 1
+        if cpt == 1:
+            continue
+       
+        XE131M_dict[dateutil.parser.parse(row[0])] = row[1]
+        XE133M_dict[dateutil.parser.parse(row[0])] = row[2]
+        XE133_dict[dateutil.parser.parse(row[0])]  = row[3]
+    
+    #print("dict = %s\n"%(res_dict))
+    return  (XE131M_dict, XE133M_dict, XE133_dict)
 
 if __name__ == '__main__':
+    #test_read_csv()
     test_my_plot()
