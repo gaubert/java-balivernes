3f2810ef0967072fe02658876b887d12b5152460
aubert <guillaume.aubert@gmail.com>
2009-09-21 13:03:58 +0200

    working version

--- RNpicker/src/ctbto/sandbox/matplotlib_test.py
+++ RNpicker/src/ctbto/sandbox/matplotlib_test.py
@@ -166,45 +166,63 @@ def test_my_plot():
     
     # figure
     fig = plt.figure()
+    # change the figure margins
+    fig.subplots_adjust(left=0.025, bottom=None, right=None, wspace=None, hspace=None)
+    
+    #send the figure size
+    fig.set_size_inches([17., 6.])
+    
     plot1 = fig.add_subplot(212)
     
     # create plot for XE133
-    plot1.plot_date(pylab.date2num(sorted_dates), XE133_sorted_values, marker='^', color='r') 
+    plot1.plot_date(pylab.date2num(sorted_dates), XE133_sorted_values, marker='^', color='y', label='xe133') 
     
-    #plot2.plot_date(pylab.date2num(sorted_dates), sorted_values, marker='^') 
-    # format
-     
-    #xaxis = plot1.gca().xaxis
+    #add legend
+    plt.legend(shadow=True, fancybox=True, bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
+    
+    leg = plot1.get_legend() # all the text.Text instance in the legend
+    for ltext in leg.get_texts():
+        ltext.set_fontsize('small')
+
     xaxis1 = plot1.xaxis
-    
-    # define xaxis beg and end limits
-    #begin = dateutil.parser.parse('2009-07-31 00:00')
-    #end   = dateutil.parser.parse('2009-08-03 00:00')
-    #plt.xlim( (begin,end) )
     
     # format xaxis with dates
     xaxis1.set_major_locator(MonthLocator())
     xaxis1.set_major_formatter(mdates.DateFormatter('%b-%y'))
     
+    xaxis1.get_label().set_text('Monthly Report')
+    
+    print("dir(Axis) = %s\n"%(dir(xaxis1)))
+    
     # for day formatting
     #xaxis.set_major_locator(DayLocator())
     #xaxis.set_major_formatter(mdates.DateFormatter('%y-%m-%d'))
 
-    plot1.set_ylim( (0, 1) )
+    plot1.set_ylim( (0, 20) )
+    plot1.set_xlim( (dateutil.parser.parse('01-01-2008'), dateutil.parser.parse('01-01-2009') ) )
     
-    
+    plot1.set_title("CNX22, XE-133 History")
+   
     #create plot for metastables
     plot2 = fig.add_subplot(211)
     
-    plot2.plot_date(pylab.date2num(sorted_dates), sorted_values, marker='^', color='y') 
-    plot2.plot_date(pylab.date2num(sorted_dates), sorted_values2, marker='*', color='b')
+    plot2.plot_date(pylab.date2num(sorted_dates), XE131M_sorted_values, marker='.', color='b',label='xe131m') 
+    plot2.plot_date(pylab.date2num(sorted_dates), XE133M_sorted_values, marker='.', color='r', label='xe133m')
     
     xaxis2 = plot1.xaxis
     
     xaxis2.set_major_locator(MonthLocator())
     xaxis2.set_major_formatter(mdates.DateFormatter('%b-%y'))
     
-    plot2.set_ylim( (0, 3) )
+    plot2.set_ylim( (-1, 1.5) )
+    
+    plt.legend(shadow=True, fancybox=True, bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
+    
+    leg = plot2.get_legend() # all the text.Text instance in the legend
+    for ltext in leg.get_texts():
+        ltext.set_fontsize('small')
+
+    plot2.set_title("CNX22, Metastables History")
     
      # to have tick names in diagonals
     fig.autofmt_xdate()
@@ -213,6 +231,23 @@ def test_my_plot():
     plt.savefig('/tmp/example.png',format='png')
     
     plt.show()
+
+def test_legend():
+    from matplotlib.pyplot import *
+
+    subplot(211)
+    plot([1,2,3], label="test1")
+    plot([3,2,1], label="test2")
+    #legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,ncol=2, mode="expand", borderaxespad=0.)
+
+    subplot(223)
+    plot([1,2,3], label="test1")
+    plot([3,2,1], label="test2")
+    legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+
+    show()
+
     
 def test_read_csv():
     import csv
@@ -237,5 +272,5 @@ def test_read_csv():
     return  (XE131M_dict, XE133M_dict, XE133_dict)
 
 if __name__ == '__main__':
-    #test_read_csv()
+    #test_legend()
     test_my_plot()
